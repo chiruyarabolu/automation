ca40fb359cddbbe35e8f11f9730028284645fba2
Chiranjeevi Yarabolu <chiruyarabolu@gmail.com>
2021-06-11 13:27:13 +0530

    Update generate_script.sh

--- generate_script.sh
+++ generate_script.sh
@@ -2,12 +2,12 @@
 
 echo "-- //$FILE_NAME" > migrate-script
 
-cat $FILE_NAME >> migrate-script
+cat mybatis-scripts/$FILE_NAME >> migrate-script
 
 echo "-- //@UNDO" >> migrate-script
 
 if [[ ! -z "$UNDO_FILE_NAME" ]] ; then
-    cat $UNDO_FILE_NAME >> migrate-script
+    cat mybatis-scripts/$UNDO_FILE_NAME >> migrate-script
 fi
 
 mv migrate-script $WORKSPACE/migration/scripts/`date +%Y%m%d%H%M%S`_$FILE_NAME.sql
