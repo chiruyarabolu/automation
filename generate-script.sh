af0e5c8394f4d1e79bbf67c6e3decea6fd895768
Chiranjeevi Yarabolu <chiruyarabolu@gmail.com>
2021-06-16 09:53:40 +0530

    Update generate-script.sh

--- generate-script.sh
+++ generate-script.sh
@@ -2,13 +2,13 @@
 
 if [[ ! -z "$FILE_NAME" ]] ; then
     echo "-- //$FILE_NAME" > migrate-script
-
+    sed -i "s/$SCHEMA_NAME//g" $FILE_NAME
     cat mybatis-scripts/$FILE_NAME >> migrate-script
 fi
 
 if [[ ! -z "$UNDO_FILE_NAME" ]] ; then
     echo "-- //@UNDO" >> migrate-script
-
+    sed -i "s/$SCHEMA_NAME//g" $UNDO_FILE_NAME
     cat mybatis-scripts/$UNDO_FILE_NAME >> migrate-script
 fi
 
